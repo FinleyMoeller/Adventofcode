0651ef2900b05d5c0b7b536f99469f19e3cfa2f7
Moeller <huntakuma@proton.me>
2025-01-28 12:54:22 +0100

    Frequency calculation

--- Adventofcode/Program.cs
+++ Adventofcode/Program.cs
@@ -7,23 +7,44 @@ public class Example
 {
     public static void Main()
     {
-        
-        Example.ReadLine();
+
+        Example.Day1DistanceCalculate();
+        Example.Day1Part2Calculate();
         Example.Stop();
 
     }
 
     public static void Stop()
     {
-
-        
         while (Console.ReadKey().Key != ConsoleKey.Enter) { }
     }
 
-    public static void ReadLine()
+
+    public static void Day1DistanceCalculate() {
+
+        const string fileName = "day1.txt";   // Datei finden
+        Tuple<List<int>, List<int>> data = Example.ReadFileData(fileName);  // Datei auslesen und verarbeiten zu data
+        int reult = Example.CalculateDistance(data);  // Distanz ausrechnen
+        Example.PrintResult(reult);  // Resultat wiedergeben
+
+    }
+
+
+    public static void Day1Part2Calculate()
+    {
+
+        const string fileName = "day1.txt";  // Datei finden
+        Tuple<List<int>, List<int>> data = Example.ReadFileData(fileName);  // Datei auslesen und verarbeiten
+        Dictionary<int, int> similarities = Example.FindSimilarNumbers(data);  // Gleiche Zahlen finden und in Dictionaries speichern
+        int result = Example.CalculateFrequency(similarities);
+        Example.PrintResult(similarities);  // Resultat wiedergeben
+        Example.PrintResult(result);  // Resultat wiedergeben
+    }
+
+
+    public static Tuple<List<int>, List<int>> ReadFileData(string fileName)
     {
         const Int32 BufferSize = 128;
-        const string fileName = "day1.txt";   // Datei finden
         List<int> zahl1 = new();              // neue Liste für erste Zahl aus dem Array erstellen
         List<int> zahl2 = new();              // neue Liste für zweite Zahl aus dem Array erstellen
 
@@ -44,48 +65,105 @@ public class Example
 
         }
 
+
+
+        return new Tuple<List<int>, List<int>>( zahl1, zahl2 );
+
+    }
+
+    public static int CalculateDistance(Tuple<List<int>, List<int>> data)
+    {
+        List<int> zahl1 = data.Item1;
+        List<int> zahl2 = data.Item2;
+
         // Zahlen sortieren
         zahl1.Sort();
         zahl2.Sort();
 
-        // Jeweils erste Zahl wiedergeben
-        Console.Write("Erste Zahl: " + zahl1[0]);
-        Console.WriteLine(", ");
-        Console.Write("Zweite Zahl: " + zahl2[0]);
-
-        
-        int SumOfDistance = 0;
-
+        int sumOfDistance = 0;
 
         for (int i = 0; i < zahl1.Count; i++)
         {
 
-
-            SumOfDistance += (int)Math.Abs(zahl1[i] - zahl2[i]);  // Differenz berechnen
+            sumOfDistance += (int)Math.Abs(zahl1[i] - zahl2[i]);  // Differenz berechnen
         }
 
+        return sumOfDistance;
+    }
+
+
+    public static void PrintResult(int result)
+    {
         //Resultat wiedergeben
+        Console.Write("Das Resultat ist: " + result);
         Console.WriteLine(", ");
-        Console.Write("Das Resultat ist: " + SumOfDistance);
 
-        Console.WriteLine(", ");
-        Console.Write("Press <Enter> to exit... ");
+    }
 
-
-        // Gleiche Zahlen in Listen finden
-        List<int> similar = new();
-
-        foreach (int i in zahl1)
-            
-        {
-            if (zahl2.Contains(i))
-            {
-                similar.Add(i);
-                Console.Write(similar);
-             /*   int Anzahl = similar.Count();
-                Console.Write(Anzahl); */
-            }
+    public static void PrintResult(Dictionary<int, int> result)
+    {
+        //Resultat wiedergeben
+        foreach (var item in result)
+        { 
+            Console.Write($"Zahl: {item.Key} kommt {item.Value} mal vor");  // Key = Zahl aus Dic 2, Value = Anzahl der Zahl in Dic 1
+            Console.WriteLine(" ");
+        
         }
 
     }
+
+
+    public static Dictionary<int, int> FindSimilarNumbers(Tuple<List<int>, List<int>> data)
+    {
+
+        Dictionary<int, int> HäufigkeitZahl2 = new();  // Dictionary 1 für alle Zahlen aus Zahl2 die mehrmals vorkommen, Anzahl der Zahlen
+        Dictionary<int, int> GleicheZahl = new();  // Dictionary 2 für alle Zahlen aus Zahl1 die auch in Dictionary 1 vorkommen
+        List<int> zahl1 = data.Item1;
+        List<int> zahl2 = data.Item2;
+
+        foreach (int number in zahl2)
+        {
+            if (HäufigkeitZahl2.ContainsKey(number))  // Wenn Dic 1 eine Zahl aus Zahl2 enthält dann...
+            {
+                HäufigkeitZahl2[number]++;  // true = plus 1 Häufigkeit in Dic 1
+            }
+            else
+            {
+                HäufigkeitZahl2[number] = 1;  // false = Häufigkeit auf 1 gesetzt
+            }
+        }
+        
+
+        foreach (int number in zahl1)  
+        {
+            if (HäufigkeitZahl2.ContainsKey(number))  // Wenn Dic 1 eine Zahl aus Zahl1 enthält dann...
+            {
+                if (GleicheZahl.ContainsKey(number))
+                {
+                    GleicheZahl[number] += HäufigkeitZahl2[number];  // Zahl aus Dic 1 in Dic 2 hinzufügen
+                }
+                else {
+                    GleicheZahl[number] = HäufigkeitZahl2[number];  // Zahl aus Dic 1 in Dic 2 hinzufügen
+                }
+            }
+        };
+        return GleicheZahl;
+    }
+
+
+
+    public static int CalculateFrequency(Dictionary<int, int> similarities)
+    {
+        int Produkt = 0;
+
+        foreach (var item in similarities)
+        {
+            Produkt += Math.Abs(item.Key * item.Value);
+
+
+        }
+
+        return Produkt;
+    }
+  
 }
